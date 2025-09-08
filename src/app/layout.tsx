f231acb0d15b819cc2a2028fd21664b51e3b4904
Gentrit <gentritzheg@gmail.com>
2025-09-08 16:37:34 +0200

    chore: initial commit, cleanup and basic setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Minimal Kings League IT - Home",
+  description:
+    "Panoramica minima delle squadre e dei giocatori della Kings League Italia.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="it">
+      <body className={roboto.variable}>{children}</body>
+    </html>
+  );
+}
