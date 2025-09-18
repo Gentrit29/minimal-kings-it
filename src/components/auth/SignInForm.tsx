2e57778d85119415b2ec64e7cd2f05d3a3627dd2
Gentrit <gentritzheg@gmail.com>
2025-09-18 17:54:44 +0200

    refactor(forms): implement Zod validation and UI components in SignIn, AddPresident, AddRoster, and AddTeam forms
    - UI components from shadcn

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,51 +1,92 @@
 "use client";
 
 import { useAuth } from "@/hooks";
-import { SignIn } from "@/lib/types";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import { SubmitHandler, useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/Button";
+import toast from "react-hot-toast";
+
+const formSignIn = z.object({
+  email: z.email({ message: "Email richiesta" }),
+  password: z.string({ message: "Password richiesta" }),
+});
+
+type FormSignInType = z.infer<typeof formSignIn>;
 
 export default function SignInForm() {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<SignIn>();
+  const form = useForm<FormSignInType>({
+    resolver: zodResolver(formSignIn),
+  });
   const { signInMutation } = useAuth();
 
-  const onSubmit: SubmitHandler<SignIn> = (data) => {
+  const onSubmit: SubmitHandler<FormSignInType> = (data) => {
     signInMutation(data, {
       onSuccess: () => {
         router.push("/");
+      },
+      onError: () => {
+        toast.error("Email o password non corretti");
+        form.resetField("password", { defaultValue: "" });
       },
     });
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mx-5 flex w-full max-w-lg flex-col items-start gap-2 rounded-lg border border-neutral-600 bg-neutral-800 p-6"
-    >
-      <div className="flex w-full justify-center">
-        <Image src="/favicon.png" alt="form logo" width={60} height={60} />
-      </div>
-      <label>Email</label>
-      <input
-        type="email"
-        className="w-full rounded-md border border-neutral-600 bg-neutral-800 px-4 py-2 text-white placeholder-neutral-400 transition outline-none focus:border-yellow-400 focus:ring focus:ring-yellow-400/30"
-        {...register("email", { required: true })}
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        className="w-full rounded-md border border-neutral-600 bg-neutral-800 px-4 py-2 text-white placeholder-neutral-400 transition outline-none focus:border-yellow-400 focus:ring focus:ring-yellow-400/30"
-        {...register("password", { required: true })}
-      />
-      <button
-        type="submit"
-        className="mt-6 w-full cursor-pointer rounded-md border border-neutral-600 bg-neutral-800 px-4 py-2 transition-colors duration-300 hover:border-yellow-400"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="border-border bg-card mx-5 flex w-full max-w-md flex-col gap-6 rounded-lg border p-5"
       >
-        Sign In
-      </button>
-    </form>
+        <div className="flex w-full justify-center">
+          <Image src="/favicon.png" alt="sign in logo" width={50} height={50} />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} type="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button role="submit" variant="default">
+          Sign In
+        </Button>
+      </form>
+    </Form>
   );
 }
