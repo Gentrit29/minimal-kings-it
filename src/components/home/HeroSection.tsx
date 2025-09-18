9d0ab79103436910eab73bed3d01b901723da27c
Gentrit <gentritzheg@gmail.com>
2025-09-18 18:01:26 +0200

    refactor(HeroSection): update layout and replace links with Button components

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
+import { Button } from "../ui/Button";
 
 export default function HeroSection() {
   return (
-    <section className="relative mx-4 mt-10 sm:mt-20">
-      <div className="absolute inset-0 flex justify-center">
-        <div className="h-full w-full rounded-full bg-yellow-500/20 blur-3xl"></div>
-      </div>
-      <div className="relative z-10 flex flex-col items-center justify-center space-y-6 py-40 text-center md:space-y-8">
+    <section className="flex min-h-screen w-full items-center justify-center">
+      <div className="text-foreground flex flex-col items-center justify-center space-y-6 px-5 text-center sm:px-0 md:space-y-8">
         <h1 className="text-3xl font-bold tracking-widest sm:text-4xl lg:text-6xl">
           Minimal Kings League IT
         </h1>
@@ -14,24 +12,18 @@ export default function HeroSection() {
           Scopri rapidamente tutte le squadre della Kings League Italia, con
           informazioni essenziali e divisione per ruoli.
         </p>
-        <p className="text-xs text-neutral-300 sm:text-sm">
-          * Tutte le informazioni sulle squadre e giocatori sono inserite da me.
+        <p className="text-xs sm:text-sm">
+          * Tutte le informazioni sulle squadre e giocatori sono gestite da me.
           I tempi di aggiornamento delle squadre variano in base alla mia
           disponibilità.
         </p>
-        <div className="flex flex-col gap-3 sm:flex-row sm:gap-4">
-          <Link
-            href="/teams"
-            className="rounded-lg border border-neutral-400 px-4 py-2 text-base transition-transform duration-300 hover:scale-105 focus:ring-2 focus:ring-neutral-500 focus:outline-none sm:text-lg"
-          >
-            Squadre
-          </Link>
-          <Link
-            href="/players"
-            className="rounded-lg bg-yellow-500 px-4 py-2 text-base transition-transform duration-300 hover:scale-105 hover:bg-yellow-600 focus:ring-2 focus:ring-yellow-400 focus:outline-none sm:text-lg"
-          >
-            Giocatori
-          </Link>
+        <div className="flex gap-3 sm:gap-4">
+          <Button asChild variant="default">
+            <Link href="/teams">Squadre</Link>
+          </Button>
+          <Button asChild variant="default">
+            <Link href="/players">Giocatori</Link>
+          </Button>
         </div>
       </div>
     </section>
