50fb3d0ff6d920d6c9fae6bdeb9e2b9e01632ec3
Gentrit <gentritzheg@gmail.com>
2025-09-19 17:28:40 +0200

    refactor(sign-in): move SignIn inside (auth) folder

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,8 +4,8 @@ import SignInForm from "@/components/auth/SignInForm";
 
 export default function SignIn() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
+    <main className="flex min-h-screen w-full items-center justify-center">
       <SignInForm />
-    </div>
+    </main>
   );
 }
