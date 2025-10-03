afe57be394bcfb921d63e4b03a90f8c6d11a6dcb
Gentrit <gentritzheg@gmail.com>
2025-10-03 17:49:28 +0200

    style(PresidentSkeleton): add padding

--- src/components/PresidentSkeleton.tsx
+++ src/components/PresidentSkeleton.tsx
@@ -6,7 +6,7 @@ export default function PresidentSkeleton({
   isDashboard: boolean;
 }) {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 px-8 py-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {Array.from({ length: 10 }).map((_, i) => (
         <Skeleton
           key={i}
