b3a39761507a2579a75da14268a09d2972b536f6
Gentrit <gentritzheg@gmail.com>
2025-10-06 13:38:08 +0200

    chore(utils): remove console log from isNew helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,13 +7,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-//helper function for "New" label
-//display label if the difference in days between created_at (from DB)
-//and the current day is 3 days or less
+/* helper function for "New" label 
+display label if the difference in days between created_at (from DB)
+and the current day is 3 days or less */
 export function isNew(createdAt: string) {
   const createdDate = new Date(createdAt);
   const now = new Date();
   const diffDays = differenceInDays(now, createdDate);
-  console.log(diffDays);
   return diffDays <= 3;
 }
