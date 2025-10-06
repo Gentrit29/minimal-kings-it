0499bcf10f8d43cf7b61869aded7a22a8b3d092d
Gentrit <gentritzheg@gmail.com>
2025-10-06 17:41:19 +0200

    fix(TeamRow): render logo only if available to prevent errors on missing images

--- src/components/dashboard/teams/TeamRow.tsx
+++ src/components/dashboard/teams/TeamRow.tsx
@@ -27,12 +27,14 @@ export default function TeamRow({
       <div className="flex items-center">
         <div className="flex flex-col items-start space-y-2">
           <span className="flex items-center gap-2">
-            <Image
-              src={`${team.logo}?v=${Date.now()}`}
-              alt={team.name}
-              width={35}
-              height={35}
-            />
+            {team.logo && (
+              <Image
+                src={`${team.logo}?v=${Date.now()}`}
+                alt={team.name}
+                width={35}
+                height={35}
+              />
+            )}
             {team.name}
           </span>
           <span className="ml-2 flex items-center gap-2">
