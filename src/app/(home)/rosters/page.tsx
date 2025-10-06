39313cda34ad1ba10f024e2348fc9f27f93fa095
Gentrit <gentritzheg@gmail.com>
2025-10-06 16:13:32 +0200

    feat(rosters): integrate useFilteredRoster hook in Roster page
    - component: RosterFilter.tsx to manage filter state
    - Update RosterCard mapping to render filtered roster with team

--- src/app/(home)/rosters/page.tsx
+++ src/app/(home)/rosters/page.tsx
@@ -1,23 +1,50 @@
 "use client";
 
+import RosterFilter from "@/components/RosterFilter";
 import RosterSkeleton from "@/components/RosterSkeleton";
 import RosterCard from "@/components/shared/RosterCard";
-import { useTeamsWithRosters } from "@/hooks";
+
+import { useFilteredRoster, useTeamsWithRosters } from "@/hooks";
+
 import { Roster, Team } from "@/lib/types";
+import { useState } from "react";
+
+type RosterWithTeam = Roster & { teams: Team };
+
+interface Filters {
+  teamId?: number;
+  role?: string;
+  status?: string;
+  role_field?: string;
+}
 
 export default function Rosters() {
-  const { isLoading, data } = useTeamsWithRosters();
+  const [filters, setFilters] = useState<Filters>({});
+
+  const { data: teams } = useTeamsWithRosters();
+
+  const { data, isLoading } = useFilteredRoster(
+    filters.teamId,
+    filters.role,
+    filters.status,
+    filters.role_field,
+  );
 
   if (isLoading) return <RosterSkeleton />;
 
   return (
     <main className="px-8 py-10">
+      <RosterFilter
+        teams={teams ?? []}
+        filters={filters}
+        onChange={(newFilters) =>
+          setFilters((prev) => ({ ...prev, ...newFilters }))
+        }
+      />
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {data?.flatMap((team: Team) =>
-          team.roster?.map((r: Roster) => (
-            <RosterCard key={r.id} roster={r} team={team} />
-          )),
-        )}
+        {data?.map((r: RosterWithTeam) => (
+          <RosterCard key={r.id} roster={r} team={r.teams} />
+        ))}
       </div>
     </main>
   );
---
+++ src/components/RosterFilter.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { Team } from "@/lib/types";
+import { Button } from "./ui/Button";
+
+type RosterFilterProps = {
+  teams: Team[];
+  filters: {
+    teamId?: number;
+    role?: string;
+    status?: string;
+    role_field?: string;
+  };
+  onChange: (filters: {
+    teamId?: number;
+    role?: string;
+    status?: string;
+    role_field?: string;
+  }) => void;
+};
+
+export default function RosterFilter({
+  teams,
+  onChange,
+  filters,
+}: RosterFilterProps) {
+  const { teamId, role, status, role_field } = filters;
+
+  return (
+    <div className="bg-card border-border mb-5 flex gap-4 rounded-md border p-2">
+      <Select
+        value={teamId?.toString()}
+        onValueChange={(val) => onChange({ teamId: Number(val) })}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Seleziona Team" />
+        </SelectTrigger>
+        <SelectContent>
+          {teams.map((team) => (
+            <SelectItem key={team.id} value={team.id!.toString()}>
+              {team.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select value={role} onValueChange={(val) => onChange({ role: val })}>
+        <SelectTrigger>
+          <SelectValue placeholder="Selezione Posizione" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="player">Player</SelectItem>
+          <SelectItem value="coach">Coach</SelectItem>
+          <SelectItem value="staff">Staff</SelectItem>
+        </SelectContent>
+      </Select>
+      {role === "player" && (
+        <>
+          <Select
+            value={status}
+            onValueChange={(val) => onChange({ status: val })}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Seleziona Stato" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="wildcard">Wildcard</SelectItem>
+              <SelectItem value="draft">Draft</SelectItem>
+              <SelectItem value="transfer">Transfer</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select
+            value={role_field}
+            onValueChange={(val) => onChange({ role_field: val })}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Seleziona Ruolo" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="ATT">ATT</SelectItem>
+              <SelectItem value="CC">CC</SelectItem>
+              <SelectItem value="DIF">DIF</SelectItem>
+              <SelectItem value="POR">POR</SelectItem>
+            </SelectContent>
+          </Select>
+        </>
+      )}
+      {(teamId || role || status || role_field) && (
+        <Button
+          variant="destructive"
+          onClick={() =>
+            onChange({
+              teamId: undefined,
+              role: undefined,
+              status: undefined,
+              role_field: undefined,
+            })
+          }
+        >
+          Pulisci Filtri
+        </Button>
+      )}
+    </div>
+  );
+}
