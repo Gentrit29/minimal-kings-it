5c923a61e8d16a4b71e7807b10c382c031608ccc
Gentrit <gentritzheg@gmail.com>
2025-10-07 15:29:25 +0200

    style(rosters): update grid layou

--- src/app/(home)/rosters/page.tsx
+++ src/app/(home)/rosters/page.tsx
@@ -41,7 +41,7 @@ export default function Rosters() {
           setFilters((prev) => ({ ...prev, ...newFilters }))
         }
       />
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {data?.map((r: RosterWithTeam) => (
           <RosterCard key={r.id} roster={r} team={r.teams} />
         ))}
